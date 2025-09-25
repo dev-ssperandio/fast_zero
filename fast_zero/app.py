1337eb4d2418bc41643c51e566d1612c7e9357b3
Sidney Ribeiro Sperandio <E5747475@fnis-br.com>
2025-09-25 16:59:50 -0300

    Implementando o endpoint POST.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
-def read_root():
-    return {'message': 'Hello, World!'}
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
