d41d5929b1fdef5127379eec70ffb4f804a6f100
Sidney Ribeiro Sperandio <E5747475@fnis-br.com>
2025-10-21 14:30:51 -0300

    Adicionando fixtures.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -8,6 +8,7 @@ app = FastAPI()
 
 database = []
 
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,19 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
 
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello, World!'}
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
