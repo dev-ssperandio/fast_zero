5fbe1ab26f58e3549b47d046655d34bb745ac730
Sidney Ribeiro Sperandio <E5747475@fnis-br.com>
2025-10-22 17:06:13 -0300

    Implementando a rota GET.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import UserDB, UserPublic, UserSchema
+from fast_zero.schemas import UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -16,3 +16,8 @@ def create_user(user: UserSchema):
     database.append(user_with_id)
 
     return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
